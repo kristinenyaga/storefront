26a372bce951d1cbd87da301920da9e96cd6d9bb
krisine <kristine.nyaga@student.moringaschool.com>
2024-01-18 14:15:20 +0300

    implementation of function based views

--- store/views.py
+++ store/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Product,Collection
 from .serializers import ProductSerializer,CollectionSerializer
 # Create your views here.
 
-@api_view(['GET','POST'])
+
 def products(request):
   if request.method == 'GET':
     products=Product.objects.select_related('collection').all()
