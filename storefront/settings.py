a11e2ab4718e5f4906deddb7b3c0ba72fac0d2fb
krisine <kristine.nyaga@student.moringaschool.com>
2024-01-30 05:12:33 +0300

    adds custom serializers(users_endpoint)

--- storefront/settings.py
+++ storefront/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -146,7 +146,16 @@ REST_FRAMEWORK = {
 }
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+DJOSER={
+  'SERIALIZERS':{
+      'user_create':'core.serializers.UserCreateSerializer',
+      'current_user':'core.serializers.UserSerializer',
+  
+  }
 
+}
 AUTH_USER_MODEL='core.User'
 #AUTH_USER_MODEL is used to specify the user model that will be used by the authentication system.
