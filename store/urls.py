78555d61b5a7b8cf00eee2e2b591250d5376dd37
krisine <kristine.nyaga@student.moringaschool.com>
2024-01-31 10:11:26 +0300

    registering orders

--- store/urls.py
+++ store/urls.py
@@ -10,6 +10,7 @@ router.register('customers',views.CustomerViewSet)
 router.register('products',views.ProductViewSet,basename='product')
 router.register('collections',views.CollectionViewSet)
 router.register('carts',views.CartViewSet,basename='cart')
+router.register('orders',views.OrderViewSet,basename='order')
 
 products_router=routers.NestedDefaultRouter(router,'products',lookup='product')
 products_router.register('reviews',views.ReviewViewSet,basename='product-reviews')
